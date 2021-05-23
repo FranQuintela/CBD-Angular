a364ced794329d65ca8a1c4fdab74c49000c2eb3
Fran Quintela <56025446+FranQuintela@users.noreply.github.com>
2021-05-23 12:17:01 +0200

    Last update

--- back_end/rest_api/urls.py
+++ back_end/rest_api/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from rest_api.views import *
+from rest_api.views import product
 from rest_api.views import category
 from rest_api.views import connectors
 from rest_api.views import supplier
@@ -7,8 +7,8 @@ from rest_api.views import supplier
 from django.urls import path
 urlpatterns = [
 
-    url(r'^api/products$', getAllProducts),
-    url(r'^api/products/(?P<productID>[0-9]+)$', productDetails),
+    url(r'^api/products$', product.getAllProducts),
+    url(r'^api/products/(?P<productID>[0-9]+)$', product.productDetails),
     url(r'^api/categories$', category.getAllCategories),
     url(r'^api/suppliers/(?P<supplierID>[0-9]+)$', supplier.supplierDetails),
     url(r'^api/suppliers$', supplier.getAllSuppliers),
